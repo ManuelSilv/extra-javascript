855c07a22204c687df253767ce36791f9ff13dd6
manuelsilv <manuel-silva@live.nl>
2019-08-08 08:29:44 +0200

    xtra

--- starter-code/extra-javascript.js
+++ starter-code/extra-javascript.js
@@ -1,15 +1,51 @@
 /* 1.   write a loop that counts from 0 to 100 and logs the current number to the console */
-// code here
+let i; 
+    {i=0; i <100 ; i++ 
+ console.log(i);
+}
+
 /* 2.   define a function that counts from 0 to 100 if called */
-// code here
+zeroToHundred = () => {
+  let i; 
+  if(i === true)
+    {i=0; i <100 ; i++ 
+ console.log(i);
+  }
+else {
+  break;
+  }
+}
+
 /* 3.   define a function that counts from 0 to whatever value you passed to it, call this function with 3 different arguments */
-// code here
+zeroToHundred = (number) =>{
+  let i;
+  let number;
+  for (i=0; i <number; i++){
+  
+zeroToHundred(4)
+zeroToHundred(10)
+zeroToHundred(54)
+}
+
 /* 4.   given this array: var arrayWords = ["cow", "chicken", "dog", "cat"] console log every element using a loop */
-// code here
+var arrayWords = ["cow", "chicken", "dog", "cat"];
+
+arrayLength = arrayWords.length;
+for (var a= 0;  a <arrayLength; a++){
+  console.log(arrayWords[i]);
+}
+
 /* 5.   define a function that logs every elements of the arrayWords variable. Call this function. */
-// code here
+arrayWords.arrayElement = () =>{
+console.log([0, 1, 2 ,3]);
+}
+
+arrayElement()
+
 /* 6.   define a function that logs all words of any array with words you pass to it. Declare two of those arrays. */
-// code here
+logAllWords = () => {
+
+}
 /* 7.   define a function that logs all words of any array with words you pass to it, except when the word is "cow" (use a conditional). Declare two of those arrays and call the function on them */
 // code here
 /* 8. given this array print out all names. The output should be: */
